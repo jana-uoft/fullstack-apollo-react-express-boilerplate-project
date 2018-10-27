90b838b51a3adfcca9b6c3efa6277aa859e9404f
gibtswohl <gibtswohl@gibtswosMacBook.fritz.box>
2018-10-27 02:59:13 +0200

    password fix

--- server/src/resolvers/user.js
+++ server/src/resolvers/user.js
@@ -5,80 +5,90 @@ import { AuthenticationError, UserInputError } from 'apollo-server';
 import { isAuthenticated, isAdmin } from './authorization';
 
 const createToken = async (user, secret, expiresIn) => {
-    const { _id, email, username, role } = user;
-    return await jwt.sign({ _id, email, username, role }, secret, {
-        expiresIn,
-    });
+  const { _id, email, username, role } = user;
+  return await jwt.sign({ _id, email, username, role }, secret, {
+    expiresIn,
+  });
 };
 
 export default {
-    Query: {
-        users: async (parent, args, { models }) =>
-            await models.User.find({}),
+  Query: {
+    users: async (parent, args, { models }) =>
+      await models.User.find({}),
 
-        user: async (parent, { id: _id }, { models }) =>
-            await models.User.findOne({ _id }),
+    user: async (parent, { id: _id }, { models }) =>
+      await models.User.findOne({ _id }),
 
-        me: async (parent, args, { models, me: { _id } = {} }) => {
-            if (!_id) {
-                return null;
-            }
+    me: async (parent, args, { models, me: { _id } = {} }) => {
+      if (!_id) {
+        return null;
+      }
 
-            return await models.User.findOne({ _id });
-        },
+      return await models.User.findOne({ _id });
+    },
+  },
+
+  Mutation: {
+    signUp: async (
+      parent,
+      { username, email, password },
+      { models: { User }, secret },
+    ) => {
+      let newUser = new User();
+      newUser.username = username;
+      newUser.email = email;
+      newUser.password = password;
+
+      return {
+        token: createToken(await newUser.save(), secret, '30m'),
+      };
     },
 
-    Mutation: {
-        signUp: async (
-            parent,
-            { username, email, password },
-            { models: { User }, secret },
-        ) => {
-            let newUser = new User();
-            newUser.username = username;
-            newUser.email = email;
-            newUser.password = password;
+    signIn: async (
+      parent,
+      { login, password },
+      { models: { User }, secret },
+    ) => {
+      const user = await User.findOne({
+        $or: [{ email: login }, { username: login }],
+      });
 
-            return { token: createToken(await newUser.save(), secret, '30m') };
-        },
+      if (!user) {
+        throw new UserInputError(
+          'No user found with this login credentials.',
+        );
+      }
 
-        signIn: async (
-            parent,
-            { login, password },
-            { models: { User }, secret },
-        ) => {
-            const user = await User.findOne({ $or: [{ 'email': login }, { 'username': login }] });
+      const isValid = await user.validatePassword(
+        password,
+        user.hashed_password,
+      );
 
-            if (!user) {
-                throw new UserInputError(
-                    'No user found with this login credentials.',
-                );
-            }
+      if (!isValid) {
+        throw new AuthenticationError('Invalid password.');
+      }
 
-            const isValid = await user.validatePassword(password, user.password);
-
-            if (!isValid) {
-                throw new AuthenticationError('Invalid password.');
-            }
-
-            return { token: createToken(user, secret, '30m') };
-        },
-
-        updateUser: combineResolvers(
-            isAuthenticated,
-            async (parent, { username }, { models: { User }, me: { _id } }) =>
-                await User.updateOne({ _id }, { $set: { username } }),
-        ),
-
-        deleteUser: combineResolvers(
-            isAdmin,
-            async (parent, { id: _id }, { models: { User } }) =>
-                await User.deleteOne({ _id }),
-        ),
+      return { token: createToken(user, secret, '30m') };
     },
 
-    User: {
-        messages: async (user) =>
-            (await user.populate('messages').execPopulate()).messages
-    },
+    updateUser: combineResolvers(
+      isAuthenticated,
+      async (
+        parent,
+        { username },
+        { models: { User }, me: { _id } },
+      ) => await User.updateOne({ _id }, { $set: { username } }),
+    ),
+
+    deleteUser: combineResolvers(
+      isAdmin,
+      async (parent, { id: _id }, { models: { User } }) =>
+        await User.deleteOne({ _id }),
+    ),
+  },
+
+  User: {
+    messages: async user =>
+      (await user.populate('messages').execPopulate()).messages,
+  },
 };
