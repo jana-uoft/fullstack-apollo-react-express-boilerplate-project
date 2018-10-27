d8821dd4192c71e20e6da3b4db233bb869c0c7db
rpweb <github@rp-webstudios.com>
2018-10-27 11:24:08 +0200

    run prettier

--- server/src/loaders/user.js
+++ server/src/loaders/user.js
@@ -1,9 +1,11 @@
 export const batchUsers = async (keys, models) => {
-    const users = await models.User.find({
-        _id: {
-            $in: keys,
-        },
-    });
+  const users = await models.User.find({
+    _id: {
+      $in: keys,
+    },
+  });
 
-    return keys.map(key => users.find(user => user._id.toString() === key.toString()));
+  return keys.map(key =>
+    users.find(user => user._id.toString() === key.toString()),
+  );
 };
